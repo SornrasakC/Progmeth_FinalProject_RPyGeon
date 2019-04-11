558599457d0d09f3207fc9e5446549f6a6241b78
sornrasak <sronrasak@gmail.com>
2019-04-11 18:58:01 +0700

    character

---
+++ src/logic/base/Character.java
@@ -0,0 +1,29 @@
+package logic.base;
+
+public abstract class Character
+{
+	protected String name;
+	
+	protected int level;
+	
+	protected int baseMinPhyAtk;
+	protected int baseMaxPhyAtk;
+	protected int baseMinMagAtk;
+	protected int baseMaxMagAtk;
+	protected int basePhyDef;
+	protected int baseMagDef;
+	protected int baseMaxHp;
+	protected int baseMaxMp;
+	protected int currentHp;
+	protected int currentMP;
+	
+	protected int tempMinPhyAtk;
+	protected int tempMaxPhyAtk;
+	protected int tempMinMagAtk;
+	protected int tempMaxMagAtk;
+	protected int tempPhyDef;
+	protected int tempMagDef;
+	protected int tempMaxHp;
+	protected int tempMaxMp;
+	
+}
---
+++ src/logic/logics/Player.java
@@ -0,0 +1,17 @@
+package logic.logics;
+import logic.base.Character;
+
+public class Player extends Character
+{
+	private String battleClass;
+	
+	private int exp = 0;
+	private int expToNextLevel;
+	
+	public Player(String name)
+	{
+		super();
+		this.name = name;
+		this.baseMaxPhyAtk
+	}
+}
