9a035be7208618c30c3288986faba1763fe9fdc6
sornrasak <sronrasak@gmail.com>
2019-04-25 00:01:14 +0700

    merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,13 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
-import logic.base.Magic;
-import logic.logics.Dungeon;
-import logic.logics.Player;
 
 
 public class Main
@@ -22,6 +20,7 @@ public class Main
 	public static <T> ArrayList<T> readJson(String filename, TypeToken<ArrayList<T>> typeToken) throws Exception
 	{
 		Scanner sc = new Scanner(new File(filename));
+//		Scanner sc = new Scanner(new BufferedReader(new FileReader(filename)));
 		String fileText = sc.useDelimiter("\\A").next();
 		sc.close();
 		ArrayList<T> jsonList = new Gson().fromJson(fileText, typeToken.getType() );
@@ -31,9 +30,14 @@ public class Main
 	{
 		try
 		{
-//			String filename = "MagicalMonsters.json";
-//			Scanner sc = new Scanner(new File(filename));
-			System.out.println(ClassLoader.getSystemResource("MagicalMonsters.json").toString());
+//			String filename = "file:/D:/Programming/EclipseWorkspace/Progmeth_FinalProject_RJyGeon/bin/ree.txt";
+//			String filename = "file:/D:/Programming/EclipseWorkspace/Progmeth_FinalProject_RJyGeon/bin/BossMonsters.json";
+//			String filename = "res/BossMonsters.json";
+			String filename = ClassLoader.getSystemResource("ree.txt").toString();
+//			new File(filename);
+//			Scanner sc = new Scanner(new BufferedReader(new FileReader(filename)));
+			Scanner sc = new Scanner(new File(filename));
+//			System.out.println(ClassLoader.getSystemResource("ree.txt"));
 //			Dungeon.dungeonInit();
 //			Player m = new Player("name");
 //			for(Magic i : m.getMagicToLearn())
