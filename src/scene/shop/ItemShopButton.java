05dea47f7094fa9d66747fe77ddc9804c6c1fb45
LightSeaGreen <moth_1999@live.com>
2019-04-28 19:56:24 +0700

    implement tooltip

--- src/scene/shop/ItemShopButton.java
+++ src/scene/shop/ItemShopButton.java
@@ -1,11 +1,20 @@
 package scene.shop;
 
+import java.lang.reflect.Field;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.util.Duration;
 import logic.base.Potion;
 import logic.logics.Player;
 import shops.ItemShop;
@@ -24,6 +33,25 @@ public class ItemShopButton extends Button {
 		
 		this.setGraphic(new ImageView(image));
 		this.setPadding(new Insets(4));
+		
+		//TODO More graphic
+		
+		WebView  webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        webEngine.loadContent
+        (
+    		"<h3>" + thisPotion.getName() + "</h3>" + thisPotion.getDescription()
+		);
+		
+		Tooltip tooltip = new Tooltip();
+		tooltip.setPrefSize(700, 120);
+		tooltip.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+		tooltip.setGraphic(webView);
+		
+		
+		hackTooltipStartTiming(tooltip);
+		this.setTooltip(tooltip);
+		
 	}
 	
 	public void setLogic(ItemShop itemShop){
@@ -35,6 +63,23 @@ public class ItemShopButton extends Button {
 				Player.player.buyPotion(itemShop, thisPotion);
 			}
 		});
+	}
+	
+	public static void hackTooltipStartTiming(Tooltip tooltip) {
+	    try {
+	        Field fieldBehavior = tooltip.getClass().getDeclaredField("BEHAVIOR");
+	        fieldBehavior.setAccessible(true);
+	        Object objBehavior = fieldBehavior.get(tooltip);
+
+	        Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
+	        fieldTimer.setAccessible(true);
+	        Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
+
+	        objTimer.getKeyFrames().clear();
+	        objTimer.getKeyFrames().add(new KeyFrame(new Duration(250)));
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
 	}
 
 }
